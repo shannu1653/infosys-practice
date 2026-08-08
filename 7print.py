20eb4187cfadcdb6d7b6416a9c396c5f4548ef74
shannu1653 <pentashanmukha2002@gmail.com>
2026-08-08 22:42:24 +0530

    working with print function

---
+++ 7print.py
@@ -0,0 +1,23 @@
+print("Restrt untill you success")
+#GOAL:practising for coding from scratch and important usefull functions/logics 
+#1.print with separtor
+print(10,20,30)
+print(10,20,30,sep=",")
+print(10,20,sep="*")
+print(10,"*",20)
+
+#2.end parameter-every print not goes to a new line
+print('hello',end=" ")
+print("Python")
+for i in range(5):
+    print(i,end=" ")
+print()
+
+#3.Escape Characters
+#3.1 New Line \n
+print("hello\nworld!")
+#3.2 TAB
+print("A\tB")
+print("A'   'b")
+
+print("Age="+"20")
